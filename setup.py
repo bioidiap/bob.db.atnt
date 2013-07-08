3329158d6175c30a83893a953846338b17008cce
Manuel Guenther <manuel.guenther@idiap.ch>
2013-07-08 20:02:50 +0200

    Updated bootstrap.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.atnt',
-    version='1.0.2',
+    version='1.1.0',
     description='ATNT/ORL Database Access API for Bob',
     url='http://github.com/bioidiap/bob.db.atnt',
     license='GPLv3',
