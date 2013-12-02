6d536ecdd410df0426434480f16fab73f8ce74fb
Laurent El Shafey <Laurent.El-Shafey@idiap.ch>
2013-12-02 11:41:58 +0100

    Update version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='xbob.db.atnt',
-    version='1.1.2a0',
+    version='1.1.2',
     description='ATNT/ORL Database Access API for Bob',
     url='http://github.com/bioidiap/xbob.db.atnt',
     license='GPLv3',
