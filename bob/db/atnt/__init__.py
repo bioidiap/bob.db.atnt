e7c48cc1c997146b32229f051410f37d411baccf
Manuel Guenther <manuel.guenther@idiap.ch>
2014-08-25 20:54:41 +0200

    Added get_config().

--- bob/db/atnt/__init__.py
+++ bob/db/atnt/__init__.py
@@ -27,6 +27,21 @@ http://www.cl.cam.ac.uk/research/dtg/attarchive/facedatabase.html
 from .models import File, Client
 from .query import Database
 
-__all__ = dir()
+def get_config():
+  """Returns a string containing the configuration information.
+  """
 
+  import pkg_resources
 
+  packages = pkg_resources.require(__name__)
+  this = packages[0]
+  deps = packages[1:]
+
+  retval =  "%s: %s (%s)\n" % (this.key, this.version, this.location)
+  retval += "  - python dependencies:\n"
+  for d in deps: retval += "    - %s: %s (%s)\n" % (d.key, d.version, d.location)
+
+  return retval.strip()
+
+# gets sphinx autodoc done right - don't remove it
+__all__ = [_ for _ in dir() if not _.startswith('_')]
